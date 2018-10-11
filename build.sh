7ebe5705f3184df1e695b62f138b62ffb1d166af
Martichou <martichou.andre@gmail.com>
2018-10-11 13:07:20 +0200

    Init the push feature & zip

--- build.sh
+++ build.sh
@@ -5,7 +5,20 @@ export SUBARCH=arm
 export DEFCONFIG="ace_defconfig"
 
 export AK2=${DIR}/Ak2
-export DTB=${DIR}/Dtb/dtbTool_moto
 
 make O=out ${DEFCONFIG}
 make O=out -j$(nproc --all) LOCALVERSION="-BetaPhase"
+
+mv out/arch/arm/boot/zImage ${AK2}/zImage
+
+if [[ -f ${AK2}/zImage ]]; then
+	echo -e ${LGR} "\r#############################################"
+	echo -e ${LGR} "\r############## Build competed! ##############"
+	echo -e ${LGR} "\r#############################################"
+
+    cd ${AK2}
+    zip -r9 "AceKernel-stock.zip" -x README.md -- *
+    curl --upload-file ./AceKernel-stock.zip https://transfer.sh/AceKernel
+else
+    exit 0
+fi
